940a6a2693956e36578e4706a7ffc0b7bdb0467d
Nelson Brasil <nelson.gbrasil@gmail.com>
2020-10-04 17:30:01 -0300

    commit

---
+++ plot1.R
@@ -0,0 +1,25 @@
+## Download and unzip the data
+file <- "exdata_data_household_power_consumption.zip"
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+dir <-""
+
+
+if(!file.exists(file)){
+  download.file(url,file, mode = "wb") 
+}
+
+if(!file.exists(dir)){
+  unzip("exdata_data_household_power_consumption.zip", files = NULL, exdir=".")
+}
+
+## Clean the data
+tbl <- read.table("household_power_consumption.txt", sep=';', skip=66637, nrows=2880, na    = c("?"))
+colnames(tbl) <- colnames(read.table("household_power_consumption.txt", sep=';', nrow = 1, header = TRUE))
+tbl$Date <- as.Date(tbl$Date, "%d/%m/%Y")
+tbl$Time <- as.character(tbl$Time)
+
+## Plot the data
+
+dev.copy(png,"plot1.png", width=480, height=480)
+hist(tbl$Global_active_power, col='red', xlab = "Global active power (kilowatts)", main="Global active power", ylim = c(0, 1400))
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,30 @@
+library(lubridate)
+## Download and unzip the data
+file <- "exdata_data_household_power_consumption.zip"
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+dir <-""
+
+
+if(!file.exists(file)){
+  download.file(url,file, mode = "wb") 
+}
+
+if(!file.exists(dir)){
+  unzip("exdata_data_household_power_consumption.zip", files = NULL, exdir=".")
+}
+
+## Clean the data
+tbl <- read.table("household_power_consumption.txt", sep=';', skip=66637, nrows=2880)
+colnames(tbl) <- colnames(read.table("household_power_consumption.txt", sep=';', nrow = 1, header = TRUE))
+tbl$Date <- as.Date(tbl$Date, "%d/%m/%Y")
+tbl$Time <- as.character(tbl$Time)
+tbl$DateTime <- ymd_hms(with(tbl, paste(Date, Time)))
+
+## Plot the data
+
+dev.copy(png,"plot3.png", width=480, height=480)
+with(tbl, plot(Sub_metering_1~DateTime, type='l', ylab='Energy Sub metering', xlab=''))
+with(tbl, lines(Sub_metering_2~DateTime, type='l', col='red'))
+with(tbl, lines(Sub_metering_3~DateTime, type='l', col='blue'))
+legend("topright", lty=1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
